f46fbfcaf949dbbd995377bc911fa7fe6157639e
jon deviney <developerdeviney1@gmail.com>
2020-11-19 12:44:41 -0700

    questions match

---
+++ src/components/Question.js
@@ -0,0 +1,20 @@
+/* export default function Question(props) {
+    const { question } = props
+    const answers = question.incorrect_answers.slice()
+    answers.push(question.correct_answer)
+    shuffleArray(answer)
+
+    return {
+        questions.map((name) => (
+            <li key={answer}>
+                <label>
+                    <input type='radio' name
+            </label>
+
+            </li>
+                <ul>
+                    {answers.map(answer => (
+
+                ))}
+                </ul>
+} */
--- src/components/TriviaQuestions.js
+++ src/components/TriviaQuestions.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react'
-
+//import Question from Question
 export default function TriviaQuestions(props) {
     const { category, clearSelectedCategory } = props
     const [questions, setQuestions] = useState([])
 
     useEffect(() => {
-        fetch('https://opentdb.com/api.php?amount=10&token=9309c4970258a820d41d680ebe09d27e494ac6b2c2875645ae432014fc822999')
+        fetch(`https://opentdb.com/api.php?amount=10&category=${category.id}&token=2fbcda84c0b0d24686725d8148d9a8736b80ec2c0f8a4da12b1f0cd9e05b9e9d`)
             .then(response => response.json())
             .then(data => {
                 setQuestions((data.results))
             })
     }, [category])
+
 
     return (
         <div>
@@ -20,9 +21,10 @@ export default function TriviaQuestions(props) {
             </button>
             <ul>
                 {questions.map((name) => (
-                    <li key={[name.category]}>
+                    <li key={[name.id]}>
                         <div>Question: {name.question}</div>
-
+                        <div>Correct Answers: {name.correct_answer}</div>
+                        <div>Incorrect Answers: {name.incorrect_answers}</div>
 
                     </li>
                 ))}
@@ -30,20 +32,3 @@ export default function TriviaQuestions(props) {
         </div >
     )
 }
-
-//function generateUniqueData(arrayData) {
-  //  const questionToCategory = {}
-
-/* for (const record of originalData) {
-     if (!questionToCategory[record.question]) {
-         questionToCategory[record.question] = false
-     }
-     questionToCategory[record.question]
- }*/
-
-/*const blah = []
-for (const question1 of Object.keys(questionToCategory)) {
-    blah.filter({ question: question1 })
-}
-return blah
-}*/
